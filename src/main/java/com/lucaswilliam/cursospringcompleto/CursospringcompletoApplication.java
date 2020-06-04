37fb60740bc8db9ccfe6d9c2fb11e84521a4dd6d
Lucas William <59783795+LucasJordao@users.noreply.github.com>
2020-06-04 13:38:39 -0300

    Projeto criado

---
+++ src/main/java/com/lucaswilliam/cursospringcompleto/CursospringcompletoApplication.java
@@ -0,0 +1,13 @@
+package com.lucaswilliam.cursospringcompleto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringcompletoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringcompletoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucaswilliam/cursospringcompleto/CursospringcompletoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaswilliam.cursospringcompleto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringcompletoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
