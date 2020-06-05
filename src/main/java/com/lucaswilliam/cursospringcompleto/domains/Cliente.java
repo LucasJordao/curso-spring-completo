b0992227b5a2ddcd42be5cb3bdb8dadb9f17ab1a
Lucas William <59783795+LucasJordao@users.noreply.github.com>
2020-06-05 20:48:30 -0300

    Ajustando e testando a delecao de Cliente

--- src/main/java/com/lucaswilliam/cursospringcompleto/domains/Cliente.java
+++ src/main/java/com/lucaswilliam/cursospringcompleto/domains/Cliente.java
@@ -14,6 +14,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import javax.persistence.CascadeType;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.lucaswilliam.cursospringcompleto.domains.enums.TipoCliente;
 
@@ -36,7 +38,7 @@ public class Cliente implements Serializable{
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
-	@OneToMany(mappedBy = "cliente")
+	@OneToMany(mappedBy = "cliente", cascade=CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	@JsonIgnore
